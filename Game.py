466e6d895962bc4862156c888441f7cef251bfb0
Zuzanune <Zane.carter@UCAS-edu.net>
2026-03-03 01:05:01 +0000

    Game works now.

--- Game.py
+++ Game.py
@@ -1,99 +1,95 @@
-#BH, KH, ZC 2nd game.py
 import random
 def game():
     print("Welcome to Tic-Tac-Toe!")
-    token = input("Would you like to be X, or O?")
+    token = input("Would you like to be X, or O? ").strip().upper()
     if token == "X":
         user_token = "X"
+        computer_token = "O"
     else:
-        user_token = "Y"
-    board = [1,2,3,4,5,6,7,8,9]
-    token = [0,0,0,0,0,0,0,0,0]
-    count = 0
-    game_data = { "scores" : {"win_1" : [board[0],board[1],board[2]], "win_2" : [board[3],board[4],board[5]], "win_3" : [board[6],board[7],board[8]], "win_4" : [board[0],board[3],board[6]], "win_5" : [board[1],board[4],board[7]], "win_6" : [board[2],board[5],board[8]], "win_7" : [board[0],board[4],board[8]], "win_8" : [board[2],board[4],board[6]]} }
-    start = 1
-    if user_token == "X":
-        computer_token = "Y"
-    else:
-        user_token = "Y"
+        user_token = "O"
         computer_token = "X"
-    def win_check(token):
-        for win in game_data:
-            pos = 0
-        for check in win:
-            if check == token:
-                pos += 1
-        if pos == 3:
-            return True
-        else:
-            return False
-        
-        
+    board = [str(i) for i in range(1, 10)]
+    game_data = {
+        "scores": {
+            "win_1": [0, 1, 2],
+            "win_2": [3, 4, 5],
+            "win_3": [6, 7, 8],
+            "win_4": [0, 3, 6],
+            "win_5": [1, 4, 7],
+            "win_6": [2, 5, 8],
+            "win_7": [0, 4, 8],
+            "win_8": [2, 4, 6]}}
+
+    def print_board():
+        print(f"{board[0]} {board[1]} {board[2]}")
+        print(f"{board[3]} {board[4]} {board[5]}")
+        print(f"{board[6]} {board[7]} {board[8]}")
+        print()
+
+    def win_check(symbol):
+        for combo in game_data["scores"].values():
+            a, b, c = combo
+            if board[a] == board[b] == board[c] == symbol:
+                return True
+        return False
+
+    def resetboard():
+        return [i for i, v in enumerate(board) if v not in ("X", "O")]
+
     def player_turn():
-        print (token[0], token[1], token[2])
-        print (token[3], token[4], token[5])
-        print (token[6], token[7], token[8])
+        print_board()
         while True:
-            choice = int(input("please select the index of the square you choose:  "))
-            if token.index(choice - 1) not in [0,1,2,3,4,5,6,7,8,]:
-                token[choice - 1] = user_token
-            else:
-                print ("that is not a valid square")
+            try:
+                choice = int(input("Please select the index of the square you choose (1–9): "))
+            except ValueError:
+                print("Please type a number between 1 and 9.")
                 continue
+
+            if choice < 1 or choice > 9:
+                print("That is not a valid square.")
+                continue
+
+            idx = choice - 1
+            if board[idx] in ("X", "O"):
+                print("That square is already taken.")
+                continue
+
+            board[idx] = user_token
             break
 
-        if win_check(user_token):
-            user_has_won = True
-            return user_has_won
-        else:
-            return False
+        return win_check(user_token)
 
-        
-        #win_check(user_token)
     def computer_turn():
-        computer_luck = random.randint(1,3)
-        chance_dict = {}
-        if computer_luck == 1:
-            for item in board:
-                if item != "X" or item != "Y":
-                    item_index = board.index(item)
-                    chance_dict[item] = item_index
-            selection = random.choice(list(chance_dict.values()))
-            for item in token:
-                if token.index(item) == selection:
-                    token[item] = computer_token
-        else:
-            c_win = []
-            for i in game_data["scores"]:
-                inti = 0
-                for base in i:
-                    if base == computer_token:
-                        inti += 1
-                if inti > 0:
-                    c_win.append(base)
-            try:
-                com = random.choice(c_win)
-            except IndexError:
+        available = resetboard()
+        if not available:
+            return False
+        idx = random.choice(available)
+        board[idx] = computer_token
+        print(f"Computer chooses square {idx + 1}")
+        print_board()
+        return win_check(computer_token)
 
-                error = True
-            if token.index(com) == computer_token:
-                if win_check(computer_token):
-                    computer_has_won = True
-                    return computer_has_won
-                else:
-                    return False
-        
-    
-    while 'true':
-        if start == 1:
+    count = 0
+    current = "player"
+
+    while True:
+        if not resetboard():
+            print("It's a tie! No more moves left.")
+            break
+
+        if current == "player":
             if player_turn():
-                print (f"You Won! it took you {count} turns to defeat the computer")
+                count += 1
+                print(f"You won! It took you {count} turns to defeat the computer.")
+                break
             count += 1
-            start = 2
-        if start == 2:
+            current = "computer"
+        else:
             if computer_turn():
-                print (f"the computer defeated you! it took them {count} turns")
+                count += 1
+                print(f"The computer defeated you! It took {count} turns.")
+                break
             count += 1
-            start = 1
-                
+            current = "player"
+    return count
 game()
